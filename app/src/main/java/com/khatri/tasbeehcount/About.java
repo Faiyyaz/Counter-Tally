60c12552525fa25d8f4eadb1fe0bcc33a7db57f3
Faiyyaz <kfaiyyaz93@gmail.com>
2016-10-10 12:24:05 +0530

    Vibration Bug Fix

---
+++ app/src/main/java/com/khatri/tasbeehcount/About.java
@@ -0,0 +1,16 @@
+package com.khatri.tasbeehcount;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Faiyyaz on 17-Sep-16.
+ */
+public class About extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
--- app/src/main/java/com/khatri/tasbeehcount/MainActivity.java
+++ app/src/main/java/com/khatri/tasbeehcount/MainActivity.java
@@ -2,13 +2,17 @@ package com.khatri.tasbeehcount;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Vibrator;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,9 +22,9 @@ import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv, tv1;
+    private TextView tv;
     private EditText sc;
-    private Button count, reset, target, exit;
+    private Button count, reset, exit;
     private int counter, s;
     private String set;
     private ToggleButton toggle,vibtoggle;
@@ -30,17 +34,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.tv);
-        tv1 = (TextView) findViewById(R.id.tv1);
         exit = (Button) findViewById(R.id.exit);
         count = (Button) findViewById(R.id.count);
         reset = (Button) findViewById(R.id.reset);
         sc = (EditText) findViewById(R.id.setcount);
-        target = (Button) findViewById(R.id.set);
         toggle=(ToggleButton)findViewById(R.id.toggle);
         vibtoggle=(ToggleButton)findViewById(R.id.vibtoggle);
 
-        toggle.setChecked(false);
-        vibtoggle.setChecked(false);
+        loadSharedPreferences();
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Instructions");
@@ -62,6 +63,16 @@ public class MainActivity extends AppCompatActivity {
         count.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                if(toggle.isChecked()) {
+                    mp.start();
+                }
+
+                if(vibtoggle.isChecked()){
+                    Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                    vibe.vibrate(50);
+                }
+
                 SharedPreferences countSettings = getSharedPreferences("count", 0);
                 int counter = countSettings.getInt("counts", 0);
                 counter++;
@@ -73,60 +84,12 @@ public class MainActivity extends AppCompatActivity {
                     setCount();
                 }
 
+                if (counter %100 == 0){
+                    Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                    vibe.vibrate(2000);
+                }
+
                 tv.setText(String.valueOf(counter));
-            }
-        });
-
-        vibtoggle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                boolean vibon=((ToggleButton)view).isChecked();
-                if(vibon){
-                    count.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            mp.start();
-                            Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-                            vibe.vibrate(50);
-                            SharedPreferences countSettings = getSharedPreferences("count", 0);
-                            int counter = countSettings.getInt("counts", 0);
-                            counter++;
-                            final SharedPreferences.Editor edit = countSettings.edit();
-                            edit.putInt("counts", counter);
-                            edit.commit();
-
-                            if (counter == Integer.parseInt(sc.getText().toString())) {
-                                setCount();
-                            }
-
-                            if (counter % 100 == 0) {
-                                vibe.vibrate(200);
-                            }
-
-                            tv.setText(String.valueOf(counter));
-                        }
-                    });
-
-                }
-                else{
-                    count.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            mp.start();
-                            SharedPreferences countSettings = getSharedPreferences("count", 0);
-                            int counter = countSettings.getInt("counts", 0);
-                            counter++;
-                            final SharedPreferences.Editor edit = countSettings.edit();
-                            edit.putInt("counts", counter);
-                            edit.commit();
-
-                            if (counter == Integer.parseInt(sc.getText().toString())) {
-                                setCount();
-                            }
-                            tv.setText(String.valueOf(counter));
-                        }
-                    });
-                }
             }
         });
 
@@ -137,92 +100,60 @@ public class MainActivity extends AppCompatActivity {
                 final SharedPreferences.Editor edit = countSettings.edit();
                 edit.putInt("counts", 0);
                 edit.commit();
+                sc.setText("0");
                 tv.setText(String.valueOf(counter = 0));
             }
         });
-        target.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tv1.setText(sc.getText().toString());
-            }
-        });
+
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        switch (which) {
-                            case DialogInterface.BUTTON_POSITIVE:
-                                dialog.dismiss();
-                                System.exit(0);
-                                finish();
-                                break;
-
-                            case DialogInterface.BUTTON_NEGATIVE:
-                                dialog.dismiss();
-                                break;
-                        }
-                    }
-                };
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setMessage("Do you want to Exit?").setPositiveButton("Yes", dialogClickListener)
-                        .setNegativeButton("No", dialogClickListener).show();
+                exit();
             }
         });
 
-        toggle.setOnClickListener(new View.OnClickListener() {
+        toggle.setOnClickListener(new ToggleButton.OnClickListener() {
             @Override
             public void onClick(View view) {
-                boolean on=((ToggleButton)view).isChecked();
-                if(on){
-                    count.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            mp.start();
-                            SharedPreferences countSettings = getSharedPreferences("count", 0);
-                            int counter = countSettings.getInt("counts", 0);
-                            counter++;
-                            final SharedPreferences.Editor edit = countSettings.edit();
-                            edit.putInt("counts", counter);
-                            edit.commit();
+                SharedPreferences soundPreferences=PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                SharedPreferences.Editor editor=soundPreferences.edit();
+                editor.putBoolean("toggle",toggle.isChecked());
+                editor.commit();
+            }
+        });
 
-                            if (counter == Integer.parseInt(sc.getText().toString())) {
-                                setCount();
-                            }
-
-                            tv.setText(String.valueOf(counter));
-                        }
-                    });
-                }
-                else{
-                        count.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                SharedPreferences countSettings = getSharedPreferences("count", 0);
-                                int counter = countSettings.getInt("counts", 0);
-                                counter++;
-                                final SharedPreferences.Editor edit = countSettings.edit();
-                                edit.putInt("counts", counter);
-                                edit.commit();
-
-                                if (counter == Integer.parseInt(sc.getText().toString())) {
-                                    setCount();
-                                }
-
-                                tv.setText(String.valueOf(counter));
-                            }
-                        });
-                }
+        vibtoggle.setOnClickListener(new ToggleButton.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences vibPreferences=PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                SharedPreferences.Editor editor=vibPreferences.edit();
+                editor.putBoolean("vibtoggle",vibtoggle.isChecked());
+                editor.commit();
             }
         });
     }
 
+    private void loadSharedPreferences(){
+        SharedPreferences soundPreferences=PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        toggle.setChecked(soundPreferences.getBoolean("toggle",false));
+        SharedPreferences vibPreferences=PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        vibtoggle.setChecked(vibPreferences.getBoolean("vibtoggle",false));
+    }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if ((keyCode == KeyEvent.KEYCODE_VOLUME_UP)) {
+
+            final MediaPlayer mp = MediaPlayer.create(this, R.raw.click);
+
+            if (toggle.isChecked())
+                mp.start();
+
+            if(vibtoggle.isChecked()){
+                Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                vibe.vibrate(50);
+            }
+
             SharedPreferences countSettings = getSharedPreferences("count", 0);
             int counter = countSettings.getInt("counts", 0);
             counter++;
@@ -234,9 +165,25 @@ public class MainActivity extends AppCompatActivity {
                 setCount();
             }
 
+            if (counter %100 == 0){
+                Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                vibe.vibrate(2000);
+            }
+
             tv.setText(String.valueOf(counter));
         }
         return true;
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        SharedPreferences countSettings = getSharedPreferences("count", 0);
+        final SharedPreferences.Editor edit = countSettings.edit();
+        edit.putInt("counts", 0);
+        edit.commit();
+        sc.setText("0");
+        tv.setText(String.valueOf(counter = 0));
     }
 
     public void setCount() {
@@ -250,17 +197,18 @@ public class MainActivity extends AppCompatActivity {
                     switch (which) {
                         case DialogInterface.BUTTON_POSITIVE:
                             dialog.dismiss();
-                            count.setClickable(true);
+                            sc.setText("0");
                             break;
 
                         case DialogInterface.BUTTON_NEGATIVE:
                             dialog.dismiss();
+                            exit();
+                            sc.setText("0");
                             SharedPreferences countSettings = getSharedPreferences("count", 0);
                             final SharedPreferences.Editor edit = countSettings.edit();
                             edit.putInt("counts", 0);
                             edit.commit();
                             tv.setText(String.valueOf(counter = 0));
-                            count.setClickable(false);
                             break;
                     }
                 }
@@ -270,6 +218,43 @@ public class MainActivity extends AppCompatActivity {
             builder.setMessage("Do you want to pray more?").setPositiveButton("Yes", dialogClickListener)
                     .setNegativeButton("No", dialogClickListener).show();
         }
+    public void exit(){
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        dialog.dismiss();
+                        System.exit(0);
+                        finish();
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        dialog.dismiss();
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Do you want to Exit?").setPositiveButton("Yes", dialogClickListener)
+                .setNegativeButton("No", dialogClickListener).show();
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);//Menu Resource, Menu
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item:
+                Intent about=new Intent(MainActivity.this,About.class);
+              startActivity(about);
+        }
+        return true;
+    }
+}
 
 
